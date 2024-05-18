e225d353fa26e1bc64b25134fa7bb79ca803f50f
dinukasw <dinukasanthush50@gmail.com>
2024-05-19 00:43:27 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
+import { createStaticRouter, RouterProvider, Outlet } from "react-router-dom";
 import Menu from "./components/Menu/Menu";
 import Navbar from "./components/Navbar/Navbar";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -48,7 +48,7 @@ function App() {
         );
     };
 
-    const router = createHashRouter([
+    const router = createStaticRouter([
         {
             path: "/",
             element: <Layout />,
